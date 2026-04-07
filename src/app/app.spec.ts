41b4688ad7e3f3770cedd07f8a4c3ff5051adc31
Al3xutul02 <66384962+Al3xutul02@users.noreply.github.com>
2026-04-07 11:42:55 +0300

    Update title test for AppComponent

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Title } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -14,10 +15,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it('should have the correct page title', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('LMSFrontend');
+    const titleService = TestBed.inject(Title);
+    expect(titleService.getTitle()).toBe('LMSFrontend');
   });
 });
