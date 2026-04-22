7f930c12c3e10db7aeccf6c129fd7cea4c45ce8b
Ioana - Alexandra Tituleasa <ioanaalexandra.tituleasa@gmail.com>
2026-04-22 12:37:31 +0300

    Squashed commit of the following:
    commit 35481edd8e4b65e5f9e2dbe23d3d5c2d39aee31f
    Author: Al3xutul03 <tudor.mihai.c7w@student.ucv.ro>
    Date:   Wed Apr 22 12:05:02 2026 +0300
    
        New Frontend Themes
    
    commit 5a3eb2e01c05bf546d620b7e116c2b11de7da719
    Author: Al3xutul03 <tudor.mihai.c7w@student.ucv.ro>
    Date:   Fri Apr 17 15:28:56 2026 +0300
    
        Fixed more typos
    
    commit bbe9c532efacc9efd4e653482497bf4f8cd0dbd9
    Author: Al3xutul03 <tudor.mihai.c7w@student.ucv.ro>
    Date:   Fri Apr 17 15:24:09 2026 +0300
    
        Solved typo
    
    commit 244cca7c8428cbc5b98dde65e236256bcfd78c19
    Author: Al3xutul03 <tudor.mihai.c7w@student.ucv.ro>
    Date:   Fri Apr 17 14:55:46 2026 +0300
    
        Fixed dto interfaces to match with incoming data
    
    commit 0d5cbcac3eb43bb5cadb08e009da89c038bb257c
    Author: Al3xutul03 <tudor.mihai.c7w@student.ucv.ro>
    Date:   Wed Apr 15 20:45:51 2026 +0300
    
        Fixed data service problem with url construction

--- src/app/core/models/dtos/book.dtos.ts
+++ src/app/core/models/dtos/book.dtos.ts
@@ -1,13 +1,13 @@
 import { BookGenre, BookStatus, Dto } from "../app.models";
 
 export interface BookCreateDto extends Dto {
-    ISBN: number;
-    Title: string;
-    Author: string;
-    Description: string;
-    Genres?: BookGenre[];
-    Count: number;
-    Status: BookStatus;
+    isbn: number;
+    title: string;
+    author: string;
+    description: string;
+    genres?: BookGenre[];
+    count: number;
+    status: BookStatus;
 }
 
 export interface BookReadDto extends Dto {
@@ -23,16 +23,16 @@ export interface BookReadDto extends Dto {
 }
 
 export interface BookUpdateDto extends Dto {
-    ISBN: number;
-    Title: string;
-    Author: string;
-    Description: string;
-    Genres?: BookGenre[];
-    Count: number;
-    Status: BookStatus;
+    isbn: number;
+    title: string;
+    author: string;
+    description: string;
+    genres?: BookGenre[];
+    count: number;
+    status: BookStatus;
 }
 
 export interface BookRelationDto extends Dto {
-    ISBN: number;
-    Count: number;
+    isbn: number;
+    count: number;
 }
--- src/app/core/models/dtos/fine.dtos.ts
+++ src/app/core/models/dtos/fine.dtos.ts
@@ -1,20 +1,20 @@
 import { Dto, FineStatus } from "../app.models";
 
 export interface FineCreateDto extends Dto {
-    LoanId: number;
-    Amount: number;
+    loanId: number;
+    amount: number;
 }
 
 export interface FineReadDto extends Dto {
-    Id: number;
-    LoanId: number;
-    Amount: number;
-    Status: FineStatus;
+    id: number;
+    loanId: number;
+    amount: number;
+    status: FineStatus;
 }
 
 export interface FineUpdateDto extends Dto{
-    Id: number;
-    LoanId: number;
-    Amount: number;
-    Status: FineStatus;
+    id: number;
+    loanId: number;
+    amount: number;
+    status: FineStatus;
 }
