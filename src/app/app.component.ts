88c1a4da8af6563b624d9e22d15fff6fc09f7e61
voicanelena <168528814+voicanelena@users.noreply.github.com>
2026-05-02 16:22:56 +0300

    done with the finish request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./core/pages/shared/nav-bar/nav-bar.component";
-import { PendingRequestsComponent } from './core/pages/loan-returns/pending-request.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent, PendingRequestsComponent],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
