be6f48aa05a93ed530dd79fbfb6b7a47a74fce60
voicanelena <168528814+voicanelena@users.noreply.github.com>
2026-05-02 20:18:52 +0300

    made the overdue users page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { PendingLoansComponent } from './core/pages/loan-returns/pending-loans.component';
 import { AddBookComponent } from './core/pages/add-book/add-book.component';
 import { InventoryComponent } from './core/pages/inventory/inventory.component';
+import { OverdueUsersComponent } from './core/pages/overdue-users/overdue-users.component';
 
 export const routes: Routes = [
   { path: '',               redirectTo: 'pending-loans', pathMatch: 'full' },
   { path: 'pending-loans',  component: PendingLoansComponent },
   { path: 'add-book',       component: AddBookComponent },
-  { path: 'inventory',      component: InventoryComponent }
+  { path: 'inventory',      component: InventoryComponent },
+  { path: 'overdue-users',  component: OverdueUsersComponent }
 ];
