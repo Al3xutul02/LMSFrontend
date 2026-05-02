fdc3127b346b2c55897f61cb284643f0f2d45f9d
Al3xutul03 <tudor.mihai.c7w@student.ucv.ro>
2026-05-02 13:59:16 +0300

    Squashed commit of the following:
    commit 462076aa1c80c739f8245b6bdfb9f07670b941ef
    Author: Voican Elena <voicanelena3@gmail.com>
    Date:   Fri May 1 17:15:28 2026 +0300
    
        As a reader reserve book (#20)
    
        * Reserve page
    
        * Fixed Book Reservation
    
        * Solved the issues in Reservation page
    
        * Modified the config file
    
        ---------
    
        Co-authored-by: voicanelena <168528814+voicanelena@users.noreply.github.com>
    
    commit 9a851a01af3305e8840cb920dc3f9d474ccb8ea0
    Author: Ioana-Alexandra Tituleasa <ioanaalexandra.tituleasa@gmail.com>
    Date:   Fri May 1 13:15:39 2026 +0300
    
        Book details and availability (#19)
    
        * popup for book details
    
        * Squashed commit of the following:
    
        commit 35481edd8e4b65e5f9e2dbe23d3d5c2d39aee31f
        Author: Al3xutul03 <tudor.mihai.c7w@student.ucv.ro>
        Date:   Wed Apr 22 12:05:02 2026 +0300
    
            New Frontend Themes
    
        commit 5a3eb2e01c05bf546d620b7e116c2b11de7da719
        Author: Al3xutul03 <tudor.mihai.c7w@student.ucv.ro>
        Date:   Fri Apr 17 15:28:56 2026 +0300
    
            Fixed more typos
    
        commit bbe9c532efacc9efd4e653482497bf4f8cd0dbd9
        Author: Al3xutul03 <tudor.mihai.c7w@student.ucv.ro>
        Date:   Fri Apr 17 15:24:09 2026 +0300
    
            Solved typo
    
        commit 244cca7c8428cbc5b98dde65e236256bcfd78c19
        Author: Al3xutul03 <tudor.mihai.c7w@student.ucv.ro>
        Date:   Fri Apr 17 14:55:46 2026 +0300
    
            Fixed dto interfaces to match with incoming data
    
        commit 0d5cbcac3eb43bb5cadb08e009da89c038bb257c
        Author: Al3xutul03 <tudor.mihai.c7w@student.ucv.ro>
        Date:   Wed Apr 15 20:45:51 2026 +0300
    
            Fixed data service problem with url construction
    
        * Combined the details mini-window with the rest of the project
    
    commit 63433793529461ffedff6dc5e1522c911df8028a
    Author: Voican Elena <voicanelena3@gmail.com>
    Date:   Thu Apr 30 17:30:04 2026 +0300
    
        As a reader search for books (#18)
    
        * Search Page for books+filters
    
        * Fixed Search by filters
    
        * Refactor styles in home component for improved layout and spacing
    
        * Solved the Dashboard tab
    
        * Deleted Page Title
    
        ---------
    
        Co-authored-by: voicanelena <168528814+voicanelena@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { LoginComponent } from './core/pages/login/login.component';
 import { DashboardComponent } from './core/pages/dashboard/dashboard.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { UserRole } from './core/models/app.models';
-import { FeedComponent } from './core/pages/feed/feed.component';
+import { HomeComponent } from './core/pages/home/home.component';
+import { ReserveComponent } from './core/pages/reserve/reserve.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'feed', component: FeedComponent,
+    { path: 'home', component: HomeComponent,
         canActivate: [AuthGuard] },
+    { path: 'reserve/:isbn', component: ReserveComponent,
+        canActivate: [AuthGuard]},
     { path: 'dashboard', component: DashboardComponent,
         canActivate: [AuthGuard], data: { roles: ['administrator'] as UserRole[] } },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
--- src/app/core/pages/shared/nav-bar/nav-bar.component.ts
+++ src/app/core/pages/shared/nav-bar/nav-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from "@angular/core";
 import { AuthService } from "../../../services/auth.service";
+import { RouterLink } from "@angular/router";
  
 @Component({
   selector: 'nav-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
