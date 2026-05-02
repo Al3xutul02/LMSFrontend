a5178161691031cd3c4abca994f4730990063747
Al3xutul03 <tudor.mihai.c7w@student.ucv.ro>
2026-05-02 13:52:26 +0300

    Implemented environment variables for easy access to global variables and less 'magic values'

--- src/app/core/services/data/book.data.service.ts
+++ src/app/core/services/data/book.data.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { DataService } from './generic/data.service';
 import { BookCreateDto, BookReadDto, BookUpdateDto } from '../../models/dtos/book.dtos';
+import { env } from '../../../../environment';
 
 @Injectable({ providedIn: 'root'})
 export class BookDataService 
     extends DataService<BookCreateDto, BookReadDto, BookUpdateDto> {
   constructor() {
-    super('book');
+    super(env.endpointMap['book']);
   }
 }
--- src/app/core/services/data/loan.data.service.ts
+++ src/app/core/services/data/loan.data.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { DataService } from './generic/data.service';
 import { LoanCreateDto, LoanReadDto, LoanUpdateDto } from '../../models/dtos/loan.dtos';
+import { env } from '../../../../environment';
 
 @Injectable({ providedIn: 'root'})
 export class LoanDataService 
     extends DataService<LoanCreateDto, LoanReadDto, LoanUpdateDto> {
   constructor() {
-    super('loan');
+    super(env.endpointMap['loan']);
   }
 }
